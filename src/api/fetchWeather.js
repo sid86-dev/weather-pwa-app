446551e7874e163db7dbdc63ee277d533d2d90c2
sid86-dev <sid86harth@gmail.com>
2022-03-23 12:20:31 +0530

    bugfix

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
-const API_KEY = '57d59e0677a177f3414912ce3b807118';
+const API_KEY = process.env.REACT_APP_WEATHER_APIKEY;
 
 export const fetchWeather = async (query) => {
     const { data } = await axios.get(URL, {
